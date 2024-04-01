577f4a6d1331bf1737fb96b2805406321997ae9a
dongjun-Yi <djyi0223@gmail.com>
2024-04-01 15:31:22 +0900

    객체 생성을 수행하는 AppConfig의 객체 관리 테스트

---
+++ src/test/java/com/example/demo/container/SingletonTest.java
@@ -0,0 +1,22 @@
+package com.example.demo.container;
+
+import com.example.demo.config.AppConfig;
+import com.example.demo.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonTest {
+    @Test
+    @DisplayName("스프링이 아닌 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
